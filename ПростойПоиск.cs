6169820f904deea66a5d212c6ad375d5ddf4590f
TimPlayer21 <105927873+TimPlayer21@users.noreply.github.com>
2022-10-07 17:29:10 +0500

    Add files via upload

---
+++ ПростойПоиск.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ПростойПоиск
+{
+    internal class SimpleSearch
+    {
+        public int Search(int[] a, int x)
+        {
+            int i = 0;
+            while (i < a.Length && a[i] != x) i++;
+            if (i < a.Length)
+                return i;
+            else return -1;
+        }
+    }
+}
